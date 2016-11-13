e3194945144225d8ce569708475dc73626fabbbe
asg2016 <nefilim.star.7000@gmail.com>
2016-11-13 21:04:16 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
+sudo /etc/init.d/nginx start
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
+sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo etc/init.d/gunicorn restart

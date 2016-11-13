ed0760933a74aee6b7e8e18ddf4966446dd0a5b4
asg2016 <nefilim.star.7000@gmail.com>
2016-11-13 21:20:16 +0500

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,12 +1,3 @@
-CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web',
-    # 'python': '/usr/bin/python',
-    'args': (
-        '--bind=0.0.0.0:8080',
-        '--daemon',
-        '--workers=2',
-        '--timeout=60',
-        'hello:app',
-    ),
-}
+pythonpath = '/home/box/web'
+bind = '0.0.0.0:8080'
+workers = 4

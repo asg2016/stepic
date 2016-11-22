96cefcf1ff2200d688331e500985b3d3ec4c8127
asg2016 <nefilim.star.7000@gmail.com>
2016-11-22 05:10:24 +0500

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,11 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-		       
+urlpatterns = patterns('',		       
 	url(r'^$', 'qa.views.test', name='home'),
 	url(r'^login/$', 'qa.views.test', name='login'),
 	url(r'^signup/$', 'qa.views.test', name='signup'),
